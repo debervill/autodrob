b5c79e8c10ca72dfa2f42e9d48ffeda0cdb45de8
danamir <danamir522@gmail.com>
2018-02-25 15:23:18 +0300

    create .gitignore

--- gui/mainpage.py
+++ gui/mainpage.py
@@ -11,23 +11,28 @@ class MainWindow(wx.Frame):
         sizer_hor = wx.BoxSizer(wx.HORIZONTAL)
         sizer_vert_1 = wx.BoxSizer(wx.VERTICAL)
         sizer_vert_2 = wx.BoxSizer(wx.VERTICAL)
+
         sizer_hor.Add(sizer_vert_1)
         sizer_hor.Add(sizer_vert_2)
+
         lgn_label = wx.StaticText(self, wx.NewId(), "Login")
         pswd_label = wx.StaticText(self, wx.NewId(), "Password")
         lgn_text = wx.TextCtrl(self, wx.NewId())
         pswd_text = wx.TextCtrl(self, wx.NewId())
+
         sizer_vert_1.Add(lgn_label, flag=wx.ALL)
         sizer_vert_1.AddSpacer(10)
         sizer_vert_1.Add(pswd_label, flag=wx.ALL)
+
         sizer_vert_2.Add(lgn_text)
         sizer_vert_2.Add(pswd_text)
+
         self.SetSizer(sizer_hor)
         self.Layout()
 
 
 if __name__ == "__main__":
-    app = wx.PySimpleApp(0)
+    app = wx.App(0)
     wx.InitAllImageHandlers()
     mainWnd = MainWindow(None, -1, "")
     app.SetTopWindow(mainWnd)
