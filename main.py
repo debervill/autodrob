2ae341476bfe01887a5629671e1e1b64cf401164
danamir <danamir522@gmail.com>
2018-03-09 02:33:04 +0300

    Добавлен документ ворд для тестов. Гарантированно работает главная страница

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
+# -*- coding: utf-8 -*-
 import wx
+import wx.xrc
 
-class Page1(wx.Panel):
+
+class MainPage(wx.Frame):
+
     def __init__(self, parent):
-        wx.Panel.__init__(self, parent)
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=wx.EmptyString, pos=wx.DefaultPosition,
+                          size=wx.Size(500, 300), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
 
-        sizer_all = wx.BoxSizer(wx.HORIZONTAL)
+        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
 
+        sizer_all = wx.FlexGridSizer(2, 2, 0, 0)
+        sizer_all.SetFlexibleDirection(wx.BOTH)
+        sizer_all.SetNonFlexibleGrowMode(wx.FLEX_GROWMODE_SPECIFIED)
 
+        self.lgn_label = wx.StaticText(self, wx.ID_ANY, u"Логин", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.lgn_label.Wrap(-1)
+        sizer_all.Add(self.lgn_label, 0, wx.ALL, 5)
 
+        self.lgn_txt = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        sizer_all.Add(self.lgn_txt, 0, wx.ALL, 5)
+
+        self.pswd_label = wx.StaticText(self, wx.ID_ANY, u"Пароль", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.pswd_label.Wrap(-1)
+        sizer_all.Add(self.pswd_label, 0, wx.ALL, 5)
+
+        self.pswd_txt = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, style=wx.TE_PASSWORD)
+        sizer_all.Add(self.pswd_txt, 0, wx.ALL, 5)
+
+        self.SetSizer(sizer_all)
+        self.Layout()
+
+        self.Centre(wx.BOTH)
 
 
 app = wx.App(False)
-frame = wx.Frame(None)
-panel =Page1(frame)
+frame = MainPage(None)
 frame.Show()
 app.MainLoop()
 
