325e8451f957919a25b89d5c6bedfe04996a1ab1
danamir <danamir522@gmail.com>
2018-03-08 18:59:38 +0300

    aded mainpage

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from gui import mainpage
 import wx
 
+class Page1(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
 
+        sizer_all = wx.BoxSizer(wx.HORIZONTAL)
+
+
+
+
+
+app = wx.App(False)
+frame = wx.Frame(None)
+panel =Page1(frame)
+frame.Show()
+app.MainLoop()
 
 
